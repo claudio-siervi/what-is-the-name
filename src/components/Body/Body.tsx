b59bdc334a514ae7f3382ab865230eac9231211d
Claudio Siervi <claudio.siervi@gmail.com>
2023-04-22 16:48:14 -0300

    add simple button

--- src/components/Body/Body.tsx
+++ src/components/Body/Body.tsx
@@ -1,17 +1,37 @@
 import React from "react";
 import './Body.css';
+import Button from '../Button/Button'
 
 type BodyProps = {
     message: string;
 }
 
 function Body(props:BodyProps) {
+    let buttonLabel = 'My Label';
+    const updateButtonLabel = (newLabel: string): string => {
+        buttonLabel=newLabel
+        return newLabel
+    }
     return (
         <div className="App-body">
-            <h1>{props.message}</h1>
+
+            <h1>Aperte o Botão para gerar um nome</h1>
             <br/>
-            <h1>{props.message}</h1>
+
+            <Button
+                border="none"
+                color="pink"
+                height = "200px"
+                onClick={() => updateButtonLabel('New Label')}
+                radius = "50%"
+                width = "200px"
+                children = {buttonLabel}
+                />
             <br/>
+            <br/>
+            {/*<h1>{button_label}</h1>*/}
+            <br/>
+
         </div>
     );
 }
